db617b4e12bf24dcd4ecb771ae2b0f5d2037c35a
armando <armandomvillanueva@icloud.com>
2025-04-30 10:51:23 -0700

    feat: computer

--- components/activity.tsx
+++ components/activity.tsx
@@ -1,3 +1,4 @@
+import * as motion from "motion/react-client"
 import Link from "next/link";
 import * as React from "react";
 import GitHubCalendar from "react-github-calendar";
@@ -6,8 +7,11 @@ import GitHubCalendar from "react-github-calendar";
 
 export default function Activity() {
   return (
-    <div className="w-full flex items-center justify-center flex-col gap-2">
-      <h1 className="font-medium text-muted-foreground text-sm">💻 Building: <Link href="https://github.com/r3lays" className="text-primary/50 hover:text-primary transition-all underline">Creative</Link></h1>
+    <motion.div initial={{ opacity: 0, filter: "blur(12px)" }}
+      animate={{ opacity: 1, filter: "blur(0px)" }}
+      transition={{ duration: 0.8, ease: "easeOut" }}
+      className="w-full flex items-center justify-center flex-col gap-2">
+      <h1 className="font-medium text-muted-foreground text-sm flex gap-1">💻 Building: <p className="text-primary/50 hover:text-primary transition-all underline">Not sure yet...</p></h1>
       <div className="hidden sm:block">
         <GitHubCalendar
           hideMonthLabels
@@ -15,6 +19,6 @@ export default function Activity() {
           username="r3lays"
         />
       </div>
-    </div>
+    </motion.div>
   );
 }
